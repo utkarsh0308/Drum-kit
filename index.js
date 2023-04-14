132f739576fc29e6f04a5bcc6184a994f53265aa
utkarsh0308 <95604681+utkarsh0308@users.noreply.github.com>
2023-04-14 23:46:21 +0530

    update

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 var numberOfButtons = document.querySelectorAll(".drum").length;
 var tom1 = new Audio("sounds/tom-1.mp3"); 
+var tom2 = new Audio("sounds/tom-2.mp3");
+var tom3 = new Audio("sounds/tom-3.mp3");
+var tom4 = new Audio("sounds/tom-4.mp3");
+var kick = new Audio("sounds/kick-bass.mp3");
+var crash = new Audio("sounds/crash.mp3");
+var snare = new Audio("sounds/nsare.mp3");
 
-for(i = 0;i<numberOfButtons;i++) {
-    document.querySelectorAll(".drum")[i].addEventListener("click",handleClick);
-}
 
-function handleClick(){
+    document.getElementsByClassName("w drum").addEventListener("click",playTom1);
+
+function playTom1(){
     tom1.play();
 }
